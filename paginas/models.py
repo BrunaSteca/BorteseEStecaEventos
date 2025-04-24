01e7f61e2ef3048f75c18732ab40e97a92bab0cf
unknown <brunasteca@gmail.com>
2025-04-24 11:24:01 -0300

    criação de classes e atributos

--- paginas/models.py
+++ paginas/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+#Todas as classes DEVEM ter herança de models.Model
 # Create your models here.
+
+#Cria suas classes
+class TipoEvento(models.Model):
+    #Definir os atributos
+    nome = models.CharField(max_length=100)
+       
+       
+class LocalizacaoEvento(models.Model):
+    nome = models.CharField(max_length = 150)
+    endereco = models.CharField(max_length = 250, verbose_name = "endereço")
+    capacidadeMaxima = models.PositiveSmallIntegerField()
+    #campus = models.ForeignKey(Campus, on_delete=models.PROTECT)
+    
+class Perfil(models.Model):
+    nome = models.CharField(max_length = 150)
+    cpf = models.CharField(max_length = 100)
+    telefone = models.CharField(max_length = 100)
+    endereco = models.CharField(max_length = 250, verbose_name = "endereço")
+
+    #descricao = models.CharField(max_length=250)
+class Funcionario(models.Model):
+    nome = models.CharField(max_length = 150)
+    cargo = models.CharField(max_length = 150) 
+
+class Evento(models.Model):
+    nome = models.CharField(max_length = 150)
+    tipoEvento = models.ForeignKey(TipoEvento, on_delete=models.PROTECT)
+    dataEvento = models.DateTimeField(auto_now_add=True)
+    descricao = models.CharField(max_length = 250, verbose_name = "descrição")
+
+class Orcamento(models.Model):
+    valorPrevisto = models.DecimalField(max_digits=10,decimal_places=2)
+    valorReal = models.DecimalField(max_digits=10,decimal_places=2)
+    despesas = models.TextField()
+    evento = models.ForeignKey(TipoEvento, on_delete=models.PROTECT)
+
+    concluido = models.BooleanField(default=False)
