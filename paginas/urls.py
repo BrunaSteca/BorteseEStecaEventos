4abce0e8ee1b18fd69102992096ac421d31737fc
unknown <brunasteca@gmail.com>
2025-05-15 11:16:19 -0300

    Implementação do Update

--- paginas/urls.py
+++ paginas/urls.py
@@ -3,15 +3,27 @@ from django.urls import path
 from .views import Inicio, SobreView
 from .views import TipoEventoCreate, LocalizacaoCreate, PerfilCreate, FuncionarioCreate, EventoCreate, OrcamentoCreate
 
+from .views import TipoEventoUpdate, LocalizacaoUpdate, PerfilUpdate, FuncionarioUpdate, EventoUpdate, OrcamentoUpdate
 urlpatterns = [
-   path("index/", Inicio.as_view(), name="index"), #url para página inicial
+    
+   path("", Inicio.as_view(), name="index"), #url para página inicial
    path("sobre/", SobreView.as_view(), name="sobre"),
 
    path('adicionar/TipoEvento', TipoEventoCreate.as_view(), name = "inserir-tipo"), 
    path('adicionar/LocalizacaoEvento', LocalizacaoCreate.as_view(), name = 'inserir-localizacao'),
    path('adicionar/Perfil',PerfilCreate.as_view(), name = 'inserir-perfil'),
-   path('adicionar/Perfil',FuncionarioCreate.as_view(), name = 'inserir-funcionario'),
-   path('adicionar/Perfil',EventoCreate.as_view(), name = 'inserir-evento'),
-   path('adicionar/Perfil',OrcamentoCreate.as_view(), name = 'inserir-orcamento'),
+   path('adicionar/Funcionario',FuncionarioCreate.as_view(), name = 'inserir-funcionario'),
+   path('adicionar/Evento',EventoCreate.as_view(), name = 'inserir-evento'),
+   path('adicionar/Orcamento',OrcamentoCreate.as_view(), name = 'inserir-orcamento'),
+
+
+
+   path("editar/tipoEvento/<int:pk>/", TipoEventoUpdate.as_view(), name = "editar-tipo"), 
+   path("editar/localizacao/<int:pk>/", LocalizacaoUpdate.as_view(), name = 'editar-localizacao'),
+   path("editar/perfil/<int:pk>/", PerfilUpdate.as_view(), name= 'editar-perfil'),
+   path("editar/funcionario/<int:pk>/", FuncionarioUpdate.as_view(),  name = 'editar-funcionario'),
+   path("editar/evento/<int:pk>/", EventoUpdate.as_view(), name = 'editar-evento'),
+   path("editar/orcamento/<int:pk>/", OrcamentoUpdate.as_view(), name = 'editar-orcamento'),
+
 
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -10,45 +10,105 @@ class SobreView(TemplateView):
     template_name = 'paginas/sobre.html'   
 
 class TipoEventoCreate(CreateView):
-    template_name = 'pagina/form.html'
+    template_name = 'paginas/form.html'
     model = TipoEvento
     fields = ['nome']
     success_url = reverse_lazy('index')
-    extra_context = {'titulo' : 'Cadastrar Tipo De Evento'}
+    extra_context = {'titulo' : 'Cadastrar Tipo De Evento',
+                     'botão' : 'Cadastrar',}
 
 class LocalizacaoCreate(CreateView):
-    template_name = 'pagina/form.html'
+    template_name = 'paginas/form.html'
     model = LocalizacaoEvento
     fields = ['nome' , 'endereco']
-    success_url = reverse_lazy('sobre')
-    extra_context = {'titulo' : 'Cadastrar Localização'}
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Cadastrar Localização',
+                     'botão' : 'Cadastrar',}
  
 class PerfilCreate(CreateView):
-    template_name = 'pagina/form.html'
+    template_name = 'paginas/form.html'
     model = Perfil
     fields = ['nome','cpf','telefone', 'endereco']
-    success_url = reverse_lazy('sobre')
-    extra_context = {'titulo' : 'Cadastrar Perfil'}
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Cadastrar Perfil',
+                     'botão' : 'Cadastrar',}
 
 class FuncionarioCreate(CreateView):
-    template_name = 'pagina/form.html'
+    template_name = 'paginas/form.html'
     model = Funcionario
     fields = ['nome','cargo']
-    success_url = reverse_lazy('sobre')
-    extra_context = {'titulo' : 'Cadastrar Funcionario'}
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Cadastrar Funcionario',
+                     'botão' : 'Cadastrar',}
 
 class EventoCreate(CreateView):
-    template_name = 'pagina/form.html'
+    template_name = 'paginas/form.html'
     model = Evento
     fields = ['nome','tipoEvento', 'dataEvento']
-    success_url = reverse_lazy('sobre')
-    extra_context = {'titulo' : 'Cadastrar Evento'}
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Cadastrar Evento',
+                     'botão' : 'Cadastrar',}
 
 
 class OrcamentoCreate(CreateView):
-    template_name = 'pagina/form.html'
+    template_name = 'paginas/form.html'
     model = Orcamento
     fields = ['valorPrevisto','valorReal', 'despesas']
-    success_url = reverse_lazy('sobre')
-    extra_context = {'titulo' : 'Cadastrar Evento'}
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Cadastrar Evento',
+                     'botão' : 'Cadastrar',}
+    
+
+    ##################################################################################
+
+
+class TipoEventoUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = TipoEvento
+    fields = ['nome']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Editar o Tipo de Evento',
+                     'botão' : 'Editar',}
+    
+class LocalizacaoUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = LocalizacaoEvento
+    fields = ['nome' , 'endereco']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Editar Localização',
+                     'botão' : 'Editar',}
+ 
+    
+class PerfilUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Perfil
+    fields = ['nome','cpf','telefone', 'endereco']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Editar Perfil',
+                     'botão' : 'Editar',}
+    
+class FuncionarioUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Funcionario
+    fields = ['nome','cargo']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Editar Funcionario',
+                     'botão' : 'Editar',}
+
+class EventoUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Evento
+    fields = ['nome','tipoEvento', 'dataEvento']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Editar Evento',
+                     'botão' : 'Editar',}
+    
+class OrcamentoUpdate(UpdateView):
+    template_name = 'paginas/form.html'
+    model = Orcamento
+    fields = ['valorPrevisto','valorReal', 'despesas']
+    success_url = reverse_lazy('index')
+    extra_context = {'titulo' : 'Editar Evento',
+                     'botão' : 'Editar',}
+
  
